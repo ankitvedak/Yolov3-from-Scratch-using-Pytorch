762ed55f9c6693d008529b550042f722f9a2b665
ankitvedak <ankitvedak@wpi.edu>
2023-02-20 11:41:28 -0500

    Dataset.py updated

--- dataset.py
+++ dataset.py
@@ -5,7 +5,7 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image, ImageFile
-from utils import(
+from utils import (
     iou_width_height as iou,
     non_max_suppression as nms,
 )
@@ -19,17 +19,17 @@ class YOLODataset(Dataset):
                  img_dir,
                  label_dir,
                  anchors,
-                 image_size = 416,
-                 S = [13, 26, 52],
-                 C = 20,
-                 transforms = None):
+                 image_size=416,
+                 S=[13, 26, 52],
+                 C=20,
+                 transforms=None):
 
         self.annotations = pd.read_csv(csv_file)
         self.imf_dir = img_dir
         self.label_dir = label_dir
         self.S = S
         self.transforms = transforms
-        self.anchors = torch.tensor(anchors[0] + anchors[1] + anchors[2]) # For all 3 scales
+        self.anchors = torch.tensor(anchors[0] + anchors[1] + anchors[2])  # For all 3 scales
         self.num_anchors = self.anchors.shape[0]
         self.num_anchors_per_scales = self.num_anchors // 3
         self.ignore_iou_thresh = 0.5
@@ -49,9 +49,45 @@ class YOLODataset(Dataset):
             image = augmentation["image"]
             bboxes = augmentation["bboxes"]
 
-        target = [torch.zeros((self.num_anchors // 3, S, S, 6)) for S in self.S]
+        targets = [torch.zeros((self.num_anchors // 3, S, S, 6)) for S in self.S]
         # [probability objectness, x, y , w, h, c]
 
         for box in bboxes:
             iou_anchors = iou(torch.tensor(box[2:4]), self.anchors)
             anchor_indices = iou_anchors.argsort(deccending=True, dim=0)
+            x, y, width, height, class_label = box
+            has_anchor = [False, False, False]
+
+            for anchor_idx in anchor_indices:
+                scale_idx = anchor_idx // self.num_anchors_per_scales  # 0, 1, 2
+                anchor_on_scale = anchor_idx % self.num_anchor_per_scale  # 0, 1, 2
+                S = self.S[scale_idx]
+                i, j = int(S*y), int(S*x)  # x= 0.5, S=13 --> int(6.5) = 6 therefore it is the 6th cell
+                anchor_taken = targets[scale_idx][anchor_on_scale, i, j, 0]
+
+                if not anchor_taken and not has_anchor[scale_idx]:
+                    targets[scale_idx][anchor_on_scale, i, j, 0] = 1
+                    x_cell, y_cell = S*x - j, S*y - i
+                    width_cell, height_cell = (
+                        width*S,
+                        height+S,
+                    )
+                    box_coordinates = torch.tensor(
+                        [x_cell, y_cell, width_cell, height_cell]
+                    )
+
+                    targets[scale_idx][anchor_on_scale, i, j, 1:5] = box_coordinates
+                    targets[scale_idx][anchor_on_scale, i, j, 5] = int(class_label)
+                    has_anchor[scale_idx] = True
+
+                elif not anchor_taken and iou_anchors[anchor_idx] > self.ignore_iou_thresh:
+                    targets[scale_idx][anchor_on_scale, i, j, 0] = -1  # Ignore this predictions
+
+        return image, tuple(targets)
+
+def test():
+    pass
+
+
+if __name__ == "__main__":
+    test()
